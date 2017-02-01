59f94448de8bab22ebd15e66050ef46678bbb211
Bhargav Bhat <bhargavbhat@gmail.com>
2017-02-01 22:26:07 +0530

    Add Set Via Pointer Assembly Code

---
+++ projects/custom_asm/setviaptr.asm
@@ -0,0 +1,40 @@
+// Set RAM[100] size 10 to -1 using pointers
+
+@100
+D=A
+@arr
+M=D			// initialize base addr of array
+
+@10
+D=A
+@n
+M=D			// initialize n with size of array
+
+@i
+M=0			// initialize loop iterator with 0
+
+(LOOP)
+	@i
+	D=M
+	@n
+	D=D-M
+	@END
+	D;JEQ	// loop termination condition
+
+	@arr
+	D=M
+	@i
+	A=D+M	// load base(arr) + 1 into M
+	M=-1	// set base(arr) + 1 to -1
+
+	@i
+	M=M+1	// increment loop counter
+
+	@LOOP
+	0;JMP	// repeat
+
+(END)
+	@END
+	0;JMP	// Program termination
+
+
